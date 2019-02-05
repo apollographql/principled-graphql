d7c9aecd234a004a2dc8f3b4dd989db2ffebc064
Trevor Blades <tdblades@gmail.com>
2019-02-05 15:20:57 -0800

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  __experimentalThemes: [
+    {
+      resolve: 'gatsby-theme-apollo',
+      options: {
+        root: __dirname
+      }
+    }
+  ],
+  siteMetadata: {
+    title: 'Principled GraphQL',
+    description: 'Description here'
+  }
+};
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Layout from 'gatsby-theme-apollo/src/components/layout';
+import React from 'react';
+import Sidebar from 'gatsby-theme-apollo/src/components/sidebar';
+
+export default function Home() {
+  return (
+    <Layout>
+      <Sidebar title="principled">hello</Sidebar>
+    </Layout>
+  );
+}
