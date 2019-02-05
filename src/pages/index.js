4648a74332169b99c7219667f41b5347ef860dc5
Trevor Blades <tdblades@gmail.com>
2019-02-05 15:38:12 -0800

    Upgrade theme and use wrapper components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,34 @@
+import ContentWrapper from 'gatsby-theme-apollo/src/components/content-wrapper';
+import FlexWrapper from 'gatsby-theme-apollo/src/components/flex-wrapper';
 import Layout from 'gatsby-theme-apollo/src/components/layout';
+import PropTypes from 'prop-types';
 import React from 'react';
 import Sidebar from 'gatsby-theme-apollo/src/components/sidebar';
+import {graphql} from 'gatsby';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <Layout>
-      <Sidebar title="principled">hello</Sidebar>
+      <FlexWrapper>
+        <Sidebar title={props.data.site.siteMetadata.title}>hello</Sidebar>
+        <ContentWrapper>
+          <h1>Test</h1>
+        </ContentWrapper>
+      </FlexWrapper>
     </Layout>
   );
 }
+
+Home.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export const pageQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
