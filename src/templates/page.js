f5f22923af6546ce16e15a3fad91b2b629048198
Trevor Blades <tdblades@gmail.com>
2019-02-07 15:50:22 -0800

    Style the page footer

--- src/templates/page.js
+++ src/templates/page.js
@@ -3,6 +3,7 @@ import ContentWrapper from 'gatsby-theme-apollo/src/components/content-wrapper';
 import FlexWrapper from 'gatsby-theme-apollo/src/components/flex-wrapper';
 import Helmet from 'react-helmet';
 import Layout from 'gatsby-theme-apollo/src/components/layout';
+import LogoTitle from 'gatsby-theme-apollo/src/components/logo-title';
 import PropTypes from 'prop-types';
 import Sidebar from 'gatsby-theme-apollo/src/components/sidebar';
 import SidebarNav from 'gatsby-theme-apollo/src/components/sidebar-nav';
@@ -16,7 +17,7 @@ import {ReactComponent as OperationsIcon} from '../assets/icons/operations.svg';
 import {css, jsx} from '@emotion/core';
 import {size} from 'polished';
 
-const StyledContentWrapper = styled(ContentWrapper)({
+const OuterContentWrapper = styled.div({
   flexGrow: 1,
   overflow: 'auto'
 });
@@ -28,6 +29,15 @@ const Content = styled.div({
 const MainHeading = styled.h1({
   display: 'flex',
   alignItems: 'center'
+});
+
+const Markdown = styled.div({
+  '.anchor': {
+    color: 'inherit',
+    svg: {
+      fill: 'currentColor'
+    }
+  }
 });
 
 const iconStyles = css(size(65), {
@@ -62,6 +72,35 @@ const PageNavLinkHeading = styled.div({
 
 const PageNavLinkTitle = styled.div({
   color: colors.text1
+});
+
+const Footer = styled.footer({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: 48,
+  fontSize: 14,
+  color: colors.text1,
+  backgroundColor: colors.background
+});
+
+const Colophon = styled.span({
+  margin: '20px 0'
+});
+
+const FooterNav = styled.nav({
+  a: {
+    margin: '0 12px',
+    color: 'inherit',
+    textDecoration: 'none',
+    ':hover': {
+      textDecoration: 'underline'
+    }
+  }
+});
+
+const Divider = styled.hr({
+  width: 400
 });
 
 const anchorPattern = /<a href="([\w/#-]+)">([\w\s.,-]+)<\/a>/gm;
@@ -126,47 +165,63 @@ export default function Page(props) {
             pathname={props.location.pathname}
           />
         </Sidebar>
-        <StyledContentWrapper>
-          <Content>
-            <MainHeading>
-              {Icon && (
-                <Icon css={iconStyles} style={{backgroundColor: color}} />
-              )}
-              {title}
-            </MainHeading>
-            <hr />
-            <div
-              dangerouslySetInnerHTML={{__html: props.data.markdownRemark.html}}
-            />
-            <PageNav>
-              {previousPage && (
-                <PageNavLink to={previousPage.node.frontmatter.path}>
-                  <MdChevronLeft />
-                  <PageNavLinkText>
-                    <PageNavLinkHeading>Previous</PageNavLinkHeading>
-                    <PageNavLinkTitle>
-                      {previousPage.node.frontmatter.title}
-                    </PageNavLinkTitle>
-                  </PageNavLinkText>
-                </PageNavLink>
-              )}
-              {nextPage && (
-                <PageNavLink
-                  to={nextPage.node.frontmatter.path}
-                  style={{marginLeft: 'auto'}}
-                >
-                  <PageNavLinkText align="right">
-                    <PageNavLinkHeading>Next</PageNavLinkHeading>
-                    <PageNavLinkTitle>
-                      {nextPage.node.frontmatter.title}
-                    </PageNavLinkTitle>
-                  </PageNavLinkText>
-                  <MdChevronRight />
-                </PageNavLink>
-              )}
-            </PageNav>
-          </Content>
-        </StyledContentWrapper>
+        <OuterContentWrapper>
+          <ContentWrapper>
+            <Content>
+              <MainHeading>
+                {Icon && (
+                  <Icon css={iconStyles} style={{backgroundColor: color}} />
+                )}
+                {title}
+              </MainHeading>
+              <hr />
+              <Markdown
+                dangerouslySetInnerHTML={{
+                  __html: props.data.markdownRemark.html
+                }}
+              />
+              <PageNav>
+                {previousPage && (
+                  <PageNavLink to={previousPage.node.frontmatter.path}>
+                    <MdChevronLeft />
+                    <PageNavLinkText>
+                      <PageNavLinkHeading>Previous</PageNavLinkHeading>
+                      <PageNavLinkTitle>
+                        {previousPage.node.frontmatter.title}
+                      </PageNavLinkTitle>
+                    </PageNavLinkText>
+                  </PageNavLink>
+                )}
+                {nextPage && (
+                  <PageNavLink
+                    to={nextPage.node.frontmatter.path}
+                    style={{marginLeft: 'auto'}}
+                  >
+                    <PageNavLinkText align="right">
+                      <PageNavLinkHeading>Next</PageNavLinkHeading>
+                      <PageNavLinkTitle>
+                        {nextPage.node.frontmatter.title}
+                      </PageNavLinkTitle>
+                    </PageNavLinkText>
+                    <MdChevronRight />
+                  </PageNavLink>
+                )}
+              </PageNav>
+            </Content>
+          </ContentWrapper>
+          <Footer>
+            <LogoTitle />
+            <Colophon>Written by Geoff Schmidt</Colophon>
+            <Divider />
+            <FooterNav>
+              <a href="https://github.com/apollographql/principled-graphql">
+                View source code
+              </a>{' '}
+              | <a href="https://apollographql.com">ApolloGraphQL.com</a> |{' '}
+              <a href="https://summit.graphql.com">GraphQL Summit</a>
+            </FooterNav>
+          </Footer>
+        </OuterContentWrapper>
       </FlexWrapper>
     </Layout>
   );
