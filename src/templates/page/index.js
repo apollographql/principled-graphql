d119d4a4531fe0b8155ee85d3ee5d00248c2e7c5
Trevor Blades <tdblades@gmail.com>
2019-02-11 12:14:07 -0800

    Add twitter tags

--- src/templates/page/index.js
+++ src/templates/page/index.js
@@ -144,6 +144,11 @@ export default class Page extends Component {
           <meta property="og:title" content={title} />
           <meta property="og:description" content={description} />
           <meta property="og:image" content={ogImage} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@apollographql" />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:description" content={description} />
+          <meta name="twitter:image" content={ogImage} />
         </Helmet>
         <FlexWrapper onClick={this.onWrapperClick}>
           <Sidebar
